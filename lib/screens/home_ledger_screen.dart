b71a243e279fb3828d505e77a16a24d1442fba58
khidrhussein <husseinkhidr3@gmail.com>
2026-05-25 06:03:44 +0100

    Fix Isar build error by importing user_config in home_ledger_screen

--- lib/screens/home_ledger_screen.dart
+++ lib/screens/home_ledger_screen.dart
@@ -6,6 +6,7 @@ import '../providers/database_provider.dart';
 import '../providers/allowance_provider.dart';
 import 'package:go_router/go_router.dart';
 import '../models/transaction.dart';
+import '../models/user_config.dart';
 import 'dart:ui';
 
 class HomeLedgerScreen extends ConsumerWidget {
